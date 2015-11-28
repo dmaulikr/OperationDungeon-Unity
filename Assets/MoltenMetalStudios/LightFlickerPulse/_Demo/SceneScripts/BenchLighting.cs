820579b3af72c4af6f57b9eb6f3be3d3014e26ba
Felix Giovanni <gv.felix@yahoo.co.id>
2015-11-28 14:25:22 +0700

    Main Scene + Main Menu

---
+++ Assets/MoltenMetalStudios/LightFlickerPulse/_Demo/SceneScripts/BenchLighting.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class BenchLighting : MonoBehaviour {
+
+	private bool x = true;
+	public GameObject bench;
+	public string keyPress;
+
+	void Update () {
+		if(Input.GetKeyDown(keyPress)){
+			x = !x;
+		}
+		if(x && bench.activeInHierarchy == false){
+			bench.SetActive(true);
+		}else if(!x && bench.activeInHierarchy == true){
+			bench.SetActive(false);
+		}
+	}
+}
---
+++ Assets/MoltenMetalStudios/LightFlickerPulse/_Demo/SceneScripts/CameraAngles.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CameraAngles : MonoBehaviour {
+
+	public Camera[] angles;
+	public int onCam;
+
+
+	void Update() {
+		if(Input.GetKeyDown("v")){
+			angles[onCam].enabled = false;
+			onCam++;
+			if(onCam != angles.Length){
+				angles[onCam].enabled = true;
+			}else{
+				onCam = 0;
+				angles[0].enabled = true;
+			}
+		}
+	}
+}
+
